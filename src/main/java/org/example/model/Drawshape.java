04410ea2d01a38102787c7252b2046425247fab4
glomate00 <9145326@stud.op.edu.ua>
2025-10-23 23:19:06 +0300

    Update Drawshape.java

--- src/main/java/org/example/model/Drawshape.java
+++ src/main/java/org/example/model/Drawshape.java
@@ -1,1 +1,50 @@
+package ua.opnu.model;
 
+import java.awt.*;
+
+public class DrawShape {
+
+    public static DrawShape newInstance(int shapeType) {
+        DrawShape shape = null;
+        if (shapeType == DrawShape.SHAPE_RECTANGLE) {
+            shape = new Rectangle();
+        } else if (shapeType == DrawShape.SHAPE_ROUNDED_RECT) {
+            shape = new RoundedRectangle();
+        } else if (shapeType == Ellipse.SHAPE_ELLIPSE) { 
+            shape = new Ellipse();
+        }
+        return shape;
+    }
+
+    public static final int SHAPE_RECTANGLE = 0;
+    public static final int SHAPE_ROUNDED_RECT = 1;
+    
+    private Point startPoint;
+    private Point endPoint;
+
+    // Конструктор без параметрів
+    public DrawShape() {
+        this(new Point(0, 0), new Point(0, 0));
+    }
+
+    public DrawShape(Point startPoint, Point endPoint) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+    }
+
+    public Shape getShape() {
+        return this.getShape(startPoint, endPoint);
+    }
+
+    public Shape getShape(Point startPoint, Point endPoint) {
+        return null;
+    }
+
+    public void setStartPoint(Point startPoint) {
+        this.startPoint = startPoint;
+    }
+
+    public void setEndPoint(Point endPoint) {
+        this.endPoint = endPoint;
+    }
+}
