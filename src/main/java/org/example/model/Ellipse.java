d248541591a369b41af62174c1840926a8122e27
glomate00 <9145326@stud.op.edu.ua>
2025-10-23 23:46:51 +0300

    Update Ellipse.java

--- src/main/java/org/example/model/Ellipse.java
+++ src/main/java/org/example/model/Ellipse.java
@@ -1,28 +1,17 @@
-package ua.opnu.model;
+package org.example.model;
 
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
 
 public class Ellipse extends DrawShape {
-    
-    public static final int SHAPE_ELLIPSE = 2; // Нова константа
-    
-    public Ellipse() {
-        super();
-    }
-
-    public Ellipse(Point startPoint, Point endPoint) {
-        super(startPoint, endPoint);
-    }
 
     @Override
-    public Shape getShape(Point start, Point end) {
-      
-        int x = Math.min(start.x, end.x);
-        int y = Math.min(start.y, end.y);
-        int width = Math.abs(start.x - end.x);
-        int height = Math.abs(start.y - end.y);
-        
-        return new Ellipse2D.Double(x, y, width, height);
+    public Shape getShape(Point startPoint, Point endPoint) {
+        return new Ellipse2D.Double(
+                Math.min(startPoint.x, endPoint.x),
+                Math.min(startPoint.y, endPoint.y),
+                Math.abs(endPoint.x - startPoint.x),
+                Math.abs(endPoint.y - startPoint.y)
+        );
     }
 }
