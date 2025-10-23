158040e0638750a87bb5dd28d41e228493cc2648
glomate00 <9145326@stud.op.edu.ua>
2025-10-23 22:42:21 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        Animal dog = new Dog("Рекс");
+        Animal cat = new Cat("Мурчик");
+        System.out.println(dog.getName() + ": " + dog.makeSound());
+        System.out.println(cat.getName() + ": " + cat.makeSound());
     }
 }
-
