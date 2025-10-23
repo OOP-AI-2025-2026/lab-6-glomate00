2a037919c741e364250e1a0b9a1a237e235325a6
glomate00 <9145326@stud.op.edu.ua>
2025-10-23 23:40:16 +0300

    Update Drawshape.java

--- src/main/java/org/example/model/Drawshape.java
+++ src/main/java/org/example/model/Drawshape.java
@@ -1,28 +1,16 @@
-package ua.opnu.model;
+package org.example.model;
 
 import java.awt.*;
 
-public class DrawShape {
-
-    public static DrawShape newInstance(int shapeType) {
-        DrawShape shape = null;
-        if (shapeType == DrawShape.SHAPE_RECTANGLE) {
-            shape = new Rectangle();
-        } else if (shapeType == DrawShape.SHAPE_ROUNDED_RECT) {
-            shape = new RoundedRectangle();
-        } else if (shapeType == Ellipse.SHAPE_ELLIPSE) { 
-            shape = new Ellipse();
-        }
-        return shape;
-    }
+public abstract class DrawShape {
 
     public static final int SHAPE_RECTANGLE = 0;
     public static final int SHAPE_ROUNDED_RECT = 1;
-    
+    public static final int SHAPE_ELLIPSE = 2;
+
     private Point startPoint;
     private Point endPoint;
 
-    // Конструктор без параметрів
     public DrawShape() {
         this(new Point(0, 0), new Point(0, 0));
     }
@@ -32,13 +20,20 @@ public class DrawShape {
         this.endPoint = endPoint;
     }
 
+    public static DrawShape newInstance(int shapeType) {
+        return switch (shapeType) {
+            case SHAPE_RECTANGLE -> new RectangleShape();
+            case SHAPE_ROUNDED_RECT -> new RoundedRectangleShape();
+            case SHAPE_ELLIPSE -> new EllipseShape();
+            default -> null;
+        };
+    }
+
     public Shape getShape() {
         return this.getShape(startPoint, endPoint);
     }
 
-    public Shape getShape(Point startPoint, Point endPoint) {
-        return null;
-    }
+    public abstract Shape getShape(Point startPoint, Point endPoint);
 
     public void setStartPoint(Point startPoint) {
         this.startPoint = startPoint;
