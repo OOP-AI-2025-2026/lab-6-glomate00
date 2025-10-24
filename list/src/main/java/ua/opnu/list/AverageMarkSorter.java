1b7a9a70140ae29fe204f32dc2f3e94c1b216f00
glomate00 <9145326@stud.op.edu.ua>
2025-10-24 10:18:56 +0300

    Update AverageMarkSorter.java

--- list/src/main/java/ua/opnu/list/AverageMarkSorter.java
+++ list/src/main/java/ua/opnu/list/AverageMarkSorter.java
@@ -2,17 +2,18 @@ package ua.opnu.list;
 
 import java.util.Comparator;
 
-public class AverageGradeSorter implements Comparator<Student> {
+
+public class AverageMarkSorter implements Comparator<Student> {
     
     private final boolean ascending;
 
-    public AverageGradeSorter(boolean ascending) {
+    public AverageMarkSorter(boolean ascending) {
         this.ascending = ascending;
     }
 
     @Override
     public int compare(Student s1, Student s2) {
-        // ВИПРАВЛЕНО: Використовуємо getAvgMark()
+        
         int result = Double.compare(s1.getAvgMark(), s2.getAvgMark()); 
         return ascending ? result : -result;
     }
