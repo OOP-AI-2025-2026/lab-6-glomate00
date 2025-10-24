51dac3df6f0731b2bedd2ff8343e735eb96a00c2
glomate00 <9145326@stud.op.edu.ua>
2025-10-24 10:17:22 +0300

    Update LastNameSorter.java

--- list/src/main/java/ua/opnu/list/LastNameSorter.java
+++ list/src/main/java/ua/opnu/list/LastNameSorter.java
@@ -2,17 +2,17 @@ package ua.opnu.list;
 
 import java.util.Comparator;
 
-public class LastNameSorter implements Comparator<Student> {
+public class NameSorter implements Comparator<Student> {
     
     private final boolean ascending;
 
-    public LastNameSorter(boolean ascending) {
+    public NameSorter(boolean ascending) {
         this.ascending = ascending;
     }
 
     @Override
     public int compare(Student s1, Student s2) {
-        int result = s1.getLastName().compareTo(s2.getLastName());
+        int result = s1.getName().compareTo(s2.getName());
         return ascending ? result : -result;
     }
 }
