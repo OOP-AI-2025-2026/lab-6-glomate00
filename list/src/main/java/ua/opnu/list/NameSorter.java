e609c384753ec5640df7a3e5fd1a9f5e03e11b23
glomate00 <9145326@stud.op.edu.ua>
2025-10-24 10:12:47 +0300

    Update NameSorter.java

--- list/src/main/java/ua/opnu/list/NameSorter.java
+++ list/src/main/java/ua/opnu/list/NameSorter.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 
 public class NameSorter implements Comparator<Student> {
     
-    private final boolean ascending; // true для зростання, false для спадання
+    private final boolean ascending;
 
     public NameSorter(boolean ascending) {
         this.ascending = ascending;
@@ -13,7 +13,6 @@ public class NameSorter implements Comparator<Student> {
     @Override
     public int compare(Student s1, Student s2) {
         int result = s1.getName().compareTo(s2.getName());
-        // Якщо сортування за спаданням, інвертуємо результат
-        return ascending ? result : -result; 
+        return ascending ? result : -result;
     }
 }
